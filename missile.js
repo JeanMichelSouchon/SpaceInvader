a001a24a9e9a631b08370ba5b88086a72e345538
Jsouchon <76991749+JeanMichelSouchon@users.noreply.github.com>
2024-03-26 11:04:39 +0100

    changement img

--- missile.js
+++ missile.js
@@ -12,9 +12,32 @@ function pewPew() {
     var celluleMissile = document.querySelector('tr:nth-child(' + (posMissileY + 1) + ') td:nth-child(' + (posMissileX + 1) + ')');
     celluleMissile.innerHTML = '<img src="missile.png">'; //remplace l'image vide par le missile
 
-    // Retirer le missile après un court délai de 500 Ms
-    setTimeout(function() { celluleMissile.innerHTML = '<img src="vide.png">'; }, 500);
+    // Déplacer le missile automatiquement vers le haut
+    deplacementMissile(posMissileY, posMissileX);
 }
+
+// Fonction pour déplacer le missile vers le haut
+function deplacementMissile(y, x) {
+    // Effacer l'image de missile des coordonnées actuelles
+    var celluleMissile = document.querySelector('tr:nth-child(' + (y + 1) + ') td:nth-child(' + (x + 1) + ')');
+    celluleMissile.innerHTML = '<img src="vide.png">';
+
+    // Si le missile est à la limite supérieure du plateau, ne pas le déplacer davantage
+    if (y === 0) return;
+
+    // Écrire une image de missile aux coordonnées y-1
+    var nouvellePositionY = y - 1;
+    var nouvelleCelluleMissile = document.querySelector('tr:nth-child(' + (nouvellePositionY + 1) + ') td:nth-child(' + (x + 1) + ')');
+    nouvelleCelluleMissile.innerHTML = '<img src="missile.png">';
+
+    // Appeler la fonction deplacementMissile de façon récursive avec un délai de 200ms
+    setTimeout(function() {
+        deplacementMissile(nouvellePositionY, x);
+    }, 200);
+}
+
+// Exporter la fonction deplacementMissile pour qu'elle soit accessible depuis l'extérieur (dans ce cas, depuis app.js)
+window.deplacementMissile = deplacementMissile;
 
 // Exporter la fonction pewPew pour qu'elle soit accessible depuis l'extérieur (dans ce cas, depuis app.js)
 window.pewPew = pewPew;
--- vaisseau.js
+++ vaisseau.js
@@ -7,7 +7,7 @@ function lancerPartie() {
     // Récupération de la cellule où se trouve le vaisseau, et on défini la position du vaisseau avec les valeurs inscrit au dessus
     var celluleVaisseau = document.querySelector('tr:nth-child(' + (posShipY + 1) + ') td:nth-child(' + (posShipX + 1) + ')'); // Le sélecteur :nth-child(n) correspond à tout élément qui est le nième enfant de son parent.
     // Remplacement de l'image vide par l'image du vaisseau
-    celluleVaisseau.innerHTML = '<img src="vaisseau.png">';// on remplace l'image
+    celluleVaisseau.innerHTML = '<img src="vaisseau.gif">';// on remplace l'image
 }
 
 // Événement click sur le bouton "Jouer"
@@ -25,7 +25,7 @@ function effaceShip() {
 // Fonction pour afficher le vaisseau à sa nouvelle position
 function afficheShip() {
     var celluleVaisseau = document.querySelector('tr:nth-child(' + (posShipY + 1) + ') td:nth-child(' + (posShipX + 1) + ')');
-    celluleVaisseau.innerHTML = '<img src="vaisseau.png">'; // on change l'image ou se trouve le vaisseau par le vaisseau
+    celluleVaisseau.innerHTML = '<img src="vaisseau.gif">'; // on change l'image ou se trouve le vaisseau par le vaisseau
 }
 
 // Fonction pour déplacer le vaisseau en fonction de la touche appuyée
