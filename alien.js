19591436a67cbbbe20e59edf714a55855298f5e8
Jsouchon <jeanmichelsouchon83170@outlook.fr>
2024-04-09 09:01:44 +0200

    Update alien.js

--- alien.js
+++ alien.js
@@ -11,7 +11,7 @@ function initAlien() {
     isSens = false;
     isDown = false;
 
-    var celluleAlien = document.querySelector('tr:nth-child(' + (posAlienY) + ') td:nth-child(' + (posAlienX) + ')');
+    var celluleAlien = document.querySelector('tr:nth-child(' + (posAlienY) + ') td:nth-child(' + (posAlienX) + ')'); // Position en haut a gauche du table
     celluleAlien.innerHTML = '<img src="alien.png">';
 
     // Appeler la fonction deplaceAlien de façon récursive pour déplacer l'alien
@@ -20,53 +20,41 @@ function initAlien() {
     }, 500);
 }
 
-// Fonction pour déplacer l'alien
-function deplaceAlien(x, y) {
-    // Effacer l'alien de sa position actuelle
-    var celluleAlien = document.querySelector('tr:nth-child(' + (posAlienY) + ') td:nth-child(' + (posAlienX) + ')');
-    celluleAlien.innerHTML = '<img src="vide.png">';
-
-    // Afficher l'alien à la nouvelle position
-    var nouvelleCelluleAlien = document.querySelector('tr:nth-child(' + (y) + ') td:nth-child(' + (x) + ')');
-    nouvelleCelluleAlien.innerHTML = '<img src="alien.png">';
-
-    // Mettre à jour les variables de position de l'alien
-    posAlienX = x;
-    posAlienY = y;
-}
-
 // Fonction pour gérer le parcours de l'alien sur la grille    TRUE = gauche FALSE = Droite
 function parcoursGrille() {
 
-    if (posAlienX === 1 && isSens === true){ // anti débordement Coté Droit
-        posAlienY++;
-        deplaceAlien(posAlienX,posAlienY);
-        isSens= false;
-    }
-    if (posAlienX === largeur && isSens === false){ // anti débordement Coté Droit
-        posAlienY++;
-        deplaceAlien(posAlienX,posAlienY);
-        isSens= true;
+    // Effacer l'alien de sa position actuelle
+    var celluleAlienActuelle = document.querySelector('tr:nth-child(' + posAlienY + ') td:nth-child(' + posAlienX + ')');
+    celluleAlienActuelle.innerHTML = '<img src="vide.png">';
+
+    // Déplacer l'alien vers la droite ou la gauche en fonction de isSens
+    if (!isSens) { // Vers la droite
+        if (posAlienX < largeur) {
+            posAlienX++;
+        } else { // Changement de ligne si l'alien atteint le bord droit
+            posAlienY++;
+            isSens = true;
+        }
+    } else { // Vers la gauche
+        if (posAlienX > 1) {
+            posAlienX--;
+        } else { // Changement de ligne si l'alien atteint le bord gauche
+            posAlienY++;
+            isSens = false;
+        }
     }
 
+    // Afficher l'alien à sa nouvelle position
+    var celluleAlienNouvelle = document.querySelector('tr:nth-child(' + posAlienY + ') td:nth-child(' + posAlienX + ')');
+    celluleAlienNouvelle.innerHTML = '<img src="alien.png">';
     
-    var newX;
-    if(isSens === false ){
-        newX = posAlienX + 1;
-    }else{
-        newX = posAlienX - 1;
-    }
-    deplaceAlien(newX,posAlienY);
     
     // Appeler récursivement la fonction pour créer un déplacement continu de l'alien
-    setTimeout(parcoursGrille, 1000); // Appel toutes les secondes
-
-
-    
+    setTimeout(parcoursGrille, 1000); // Appel toutes les secondes 
 }
 
 
-// Exporter les fonctions parcoursGrille,initAlien et deplaceAlien pour qu'elles soient accessibles depuis l'extérieur
+// Exporter les fonctions parcoursGrille,initAlien et deplaceAlien pour qu'elles soient accessibles depuis l'extérieur du fichier
 window.initAlien = initAlien;
 window.deplaceAlien = deplaceAlien;
 window.parcoursGrille = parcoursGrille;
